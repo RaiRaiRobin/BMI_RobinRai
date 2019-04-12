dddedd7a219102f3ddf83259cb5ea22a788bb568
RaiRaiRobin <rai.ribin1000@gmail.com>
2019-04-12 19:55:57 +0545

    BMI

---
+++ app/src/main/java/com/example/bmi_robinrai/WeightModel.java
@@ -0,0 +1,33 @@
+package com.example.bmi_robinrai;
+
+public class WeightModel {
+
+    private Float weight,height;
+
+
+    public WeightModel(Float weight, Float height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+
+    public Float getWeight() {
+        return weight;
+    }
+
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+
+    public Float getHeight() {
+        return height;
+    }
+
+
+    public void setHeight(Float height) {
+        this.height = height;
+    }
+
+}
